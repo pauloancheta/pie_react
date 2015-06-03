55bdb6cf2ee9ee7fddfe3db15b94551a172598bd
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-06-03 13:24:16 -0700

    added menu workflow state events

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -7,7 +7,16 @@ class Menu < ActiveRecord::Base
   validates :name, presence: true
 
   workflow do
-    state :draft
-    state :available
+    state :draft do
+      event :publish, transitions_to: :available
+    end
+    state :available do
+      event :pause, transitions_to: :unavailable
+    end
+    state :unavailable do
+      event :unpause, transitions_to: :available
+      event :cancel, transitions_to: :cancelled
+    end
+    state :cancelled
   end
 end
