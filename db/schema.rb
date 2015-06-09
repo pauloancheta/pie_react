b25d86c4c6018eef3e92f9be78580cc6dcf96a43
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-06-09 11:04:27 -0700

    added ingredient model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606205423) do
+ActiveRecord::Schema.define(version: 20150609180409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 20150606205423) do
   end
 
   add_index "dishes", ["menu_id"], name: "index_dishes_on_menu_id", using: :btree
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "menus", force: :cascade do |t|
     t.string   "name"
