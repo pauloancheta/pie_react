2f3bde56d177b59c7c588c1e2f9d1a5a300c2450
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-06-09 18:30:20 -0700

    fixed services error by commenting it out.

--- app/services/menus/menu_index.rb
+++ app/services/menus/menu_index.rb
@@ -1,19 +1,19 @@
-class MenuIndex::Menu
-  include Virtus.model
-  #input
-  attribute :user, User
-  attribute :restaurant, Restaurant
+# class MenuIndex::Menu
+#   include Virtus.model
+#   #input
+#   attribute :user, User
+#   attribute :restaurant, Restaurant
 
-  #output
-  attribute :menu, Menu
+#   #output
+#   attribute :menu, Menu
 
-  def call
-    if user.is_admin
-      @menus = @restaurant.menus.all.decorate
-    else
-      @menus = @restaurant.menus.with_available_state.decorate
-    end
-  end
-  private
+#   def call
+#     if user.is_admin
+#       @menus = @restaurant.menus.all.decorate
+#     else
+#       @menus = @restaurant.menus.with_available_state.decorate
+#     end
+#   end
+#   private
 
-end
+# end
